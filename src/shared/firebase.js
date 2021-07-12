76bef7e1ecf07b58c384cdeff99a543a333b9d55
AndrewDongminYoo <ydm2790@gmail.com>
2021-07-12 22:12:51 +0900

    firebase

---
+++ src/shared/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIO_jTNxMivF_wKAogJznsHPdvlL-1kUs",
+  authDomain: "my-community-99787.firebaseapp.com",
+  projectId: "my-community-99787",
+  storageBucket: "my-community-99787.appspot.com",
+  messagingSenderId: "1020835749964",
+  appId: "1:1020835749964:web:28332368148153492e14e9",
+  measurementId: "G-E0B0P38EQW"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+export { auth };
